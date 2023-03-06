edd10e6fc7c047f38d906e2b4e7e2cc906ca9df4
Danae Lyons <118503247+Danaelyons@users.noreply.github.com>
2023-03-05 21:42:27 -0800

    Stage 2.

--- Develop/script.js
+++ Develop/script.js
@@ -5,4 +5,38 @@
       // Setting Values
       let utility = $(this).siblings('.description').val();
       let clock = $(this).parent().attr('id');
+
+  // Save in local storage.
+  localStorage.setItem(utility, clock);
   
+  // notification to present what is saved in local storage by notifying user with, 'show me!'
+  $('.notification').addClass('show me!');
+
+  // Timer to remove show me! as class after six seconds.
+  setTime(function () {
+    $('.notification').removeClass('show me!');
+  }, 6000);
+});
+
+function updateOfHour() {
+  // retrieving current number of hour.
+  let theHour = dayjs().hour();
+
+  $('.clock-block').each(function () {
+    let timedHour = parseInt($(this).attr('id').split('-')[1]);
+
+    // Making sure we've moved past the time.
+    if (timedHour < theHour) {
+      $(this).addClass('past');
+    } else if (timedHour === theHour) {
+      $(this).removeClass('past');
+      $(this).addClass('current');
+    } else {
+      $(this).removeClass('past');
+      $(this).removeClass('current');
+      $(this).addClass('future');
+    }
+  });
+}
+
+updateOfHour();
